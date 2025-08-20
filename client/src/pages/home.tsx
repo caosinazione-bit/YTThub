069d5eb27b7062216745921e5eb03990081b5be5
caosinazione-bit <caosinazione@gmail.com>
2025-08-20 15:46:46 +0200

    Add files via upload

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import ThumbnailGenerator from "@/components/thumbnail-generator";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ThumbnailGenerator />
+    </div>
+  );
+}
